060cb24c2dbf9f57b72872c3ac46f4974bb0a231
Viktor H. Morales <viktorhugomorales@gmail.com>
2021-04-15 12:33:39 -0300

    FIX email body format

--- includes/class-enico-micropay-activator.php
+++ includes/class-enico-micropay-activator.php
@@ -1,16 +1,6 @@
 <?php
 
 /**
- * Fired during plugin activation
- *
- * @link       https://enico.info
- * @since      1.5.0
- *
- * @package    Enico_Micropay
- * @subpackage Enico_Micropay/includes
- */
-
-/**
  * Fired during plugin activation.
  *
  * This class defines all code necessary to run during the plugin's activation.
@@ -30,37 +20,6 @@ class Enico_Micropay_Activator {
 	 * @since    1.5.0
 	 */
 	public static function activate() {
-		/*
-		if (get_option('enipay_value') == "")
-			update_option('enipay_value', '99.0');
-
-		if (get_option('enipay_mp_key') == "")
-			update_option('enipay_mp_key', 'XXXX');
-		
-		if (get_option('enipay_mp_url') == "")
-			update_option('enipay_mp_url', 'wwww.mercadopago.com.ar');
-		
-		if (get_option('enipay_mp_moneda') == "")
-			update_option('enipay_mp_moneda', 'ARS');
-		
-		if (get_option('enipay_version') == "")
-			update_option('enipay_version', '2.0.0');
-		
-		if (get_option('enipay_mp_prod') == "")
-			update_option('enipay_mp_prod', '0');
-
-		if (get_option('enipay_payperview_text') == "")
-			update_option('enipay_payperview_text', 'Este contenido es pago. Por favor, colabora con el medio y con el autor.');
-		
-		if (get_option('enipay_readmore_text') == "")
-			update_option('enipay_readmore_text', 'Leelo por ');
-		
-		if (get_option('enipay_email_subject') == "")
-			update_option('enipay_email_subject', '%%TITLE%%');
-		
-		if (get_option('enipay_email_body') == "")
-			update_option('enipay_email_body', '¡Hola!\r\n\r\nCompraste el artículo %%TITLE%% en %%SITE_NAME%%. Para leerlo cuando quieras, en cualquiera de tus dispositivos, ingresa aquí %%LINK%%\r\n \r\nQue disfrutes tu contenido de calidad. Saludos,\r\n\r\n%%SITE_NAME%%\r\n');
-		*/
 		
 		if (get_option('enico_default_price') == "")
 			update_option('enico_default_price', '99.0');
@@ -84,7 +43,7 @@ class Enico_Micropay_Activator {
 			update_option('enico_email_subject', '%%TITLE%%');
 		
 		if (get_option('enico_email_body') == "")
-			update_option('enico_email_body', '¡Hola!\r\n\r\nCompraste el artículo %%TITLE%% en %%SITE_NAME%%. Para leerlo cuando quieras, en cualquiera de tus dispositivos, ingresa aquí %%LINK%%\r\n \r\nQue disfrutes tu contenido de calidad. Saludos,\r\n\r\n%%SITE_NAME%%\r\n');
+			update_option('enico_email_body', '¡Hola!<p>Compraste el artículo %%TITLE%% en %%SITE_NAME%%.</p><p>Para leerlo cuando quieras, en cualquiera de tus dispositivos, ingresa aquí %%LINK%%</p><p>Que disfrutes tu contenido de calidad. Saludos,<p>%%SITE_NAME%%</p>');
 
 		// Create activation link page
 		$activation_link_slug = 'enico-activation-link';
